d7794e829a137933c7317c35e37fa2ee50666e61
kikimor <i@kikimor.ru>
2014-12-05 00:11:54 +0500

    reformat

--- Pdu/Pdu.php
+++ Pdu/Pdu.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This class generates PDU for sending SMS messages.
  *
@@ -62,10 +63,14 @@ class Pdu
 			$maxMessageLength = $this->isUnicodeMessage() ? 70 : 160;
 		}
 		$messageLength = mb_strlen($this->message, self::MESSAGE_CHARSET);
-		$ied1 = rand(1,255);
+		$ied1 = rand(1, 255);
 		$command = [];
 		for ($part = 1; $part <= ceil($messageLength / $maxMessageLength); $part++) {
-			$message = mb_substr($this->message, ($part-1) * $maxMessageLength, $maxMessageLength, self::MESSAGE_CHARSET);
+			$message = mb_substr(
+				$this->message, ($part - 1) * $maxMessageLength,
+				$maxMessageLength,
+				self::MESSAGE_CHARSET
+			);
 			$command[] =
 				$this->getSca() .
 				$this->pduType() .
@@ -110,8 +115,8 @@ class Pdu
 			(int)$this->isLongMessage(), // TP-UDHI. User data header indicator.
 			(int)$this->report, // TP-SRR. Status report request.
 			0, // TP-RD. Reject duplicates.
-			0,0, // TP-VPF. Validity Period Format.
-			0,1 // TP-MTI. Output message.
+			0, 0, // TP-VPF. Validity Period Format.
+			0, 1 // TP-MTI. Output message.
 		];
 
 		return $this->pack($config);
@@ -140,10 +145,10 @@ class Pdu
 			$number .= 'F';
 		}
 
-		for ($i = 0; $i < strlen($number); $i = $i+2) {
+		for ($i = 0; $i < strlen($number); $i = $i + 2) {
 			$first = $number[$i];
-			$number[$i] = $number[$i+1];
-			$number[$i+1] = $first;
+			$number[$i] = $number[$i + 1];
+			$number[$i + 1] = $first;
 		}
 
 		$phoneFormat = 91; // International format.
@@ -200,9 +205,12 @@ class Pdu
 		$udh = [
 			$this->isUnicodeMessage() ? '05' : '06', // Length of User Data Header (UDL), in this case 6.
 			$this->isUnicodeMessage() ? '00' : '08', // IEI. Define IED1 size (1 (00) or 2 (08) byte).
-			$this->isUnicodeMessage() ? '03' : '04', // IEDL. Length of the header. If IEI = 00 then 03, IEI = 08 then 04
+			$this->isUnicodeMessage() ? '03' : '04', // IEDL. Length of the header. IEI = 00 -> 03, IEI = 08 -> 04.
 			sprintf($this->isUnicodeMessage() ? '%02X' : '%04X', $ied1), // Reference number.
-			sprintf('%02X', ceil(mb_strlen($this->message, self::MESSAGE_CHARSET) / $maxMessageLength)), // Count of parts.
+			sprintf('%02X', ceil(mb_strlen(
+					$this->message,
+					self::MESSAGE_CHARSET
+				) / $maxMessageLength)), // Count of parts.
 			sprintf('%02X', $part), // Part of message.
 		];
 		return implode($udh);
@@ -217,15 +225,15 @@ class Pdu
 	{
 		if ($this->isUnicodeMessage()) {
 			$hex = strtoupper(bin2hex(iconv('UTF-8', 'UCS-2', $message)));
-			for ($i = 0; $i<strlen($hex); $i = $i + 4) {
+			for ($i = 0; $i < strlen($hex); $i = $i + 4) {
 				$first = [
 					0 => $hex[$i],
-					1 => $hex[$i+1],
+					1 => $hex[$i + 1],
 				];
-				$hex[$i] = $hex[$i+2];
-				$hex[$i+1] = $hex[$i+3];
-				$hex[$i+2] = $first[0];
-				$hex[$i+3] = $first[1];
+				$hex[$i] = $hex[$i + 2];
+				$hex[$i + 1] = $hex[$i + 3];
+				$hex[$i + 2] = $first[0];
+				$hex[$i + 3] = $first[1];
 			}
 			return $hex;
 		} else {
@@ -269,11 +277,12 @@ class Pdu
 	 * @param int $length
 	 * @return string
 	 */
-	private function asc2Bin($input, $length=8) {
+	private function asc2Bin($input, $length = 8)
+	{
 
 		$bin_out = '';
 		// Loop through every character in the string
-		for($charCount=0; $charCount < strlen($input); $charCount++) {
+		for ($charCount = 0; $charCount < strlen($input); $charCount++) {
 			$charAscii = ord($input{$charCount}); // ascii value of character
 			$charBinary = decbin($charAscii); // decimal to binary
 			$charBinary = str_pad($charBinary, $length, '0', STR_PAD_LEFT);
@@ -287,16 +296,17 @@ class Pdu
 	 * @param string $message
 	 * @return array
 	 */
-	private function strTo7Bit($message) {
+	private function strTo7Bit($message)
+	{
 		$message = trim($message);
-		$length = strlen( $message );
+		$length = strlen($message);
 		$i = 1;
 		$bitArray = [];
 
 		// Loop through every character in the string
 		while ($i <= $length) {
 			// Convert this character to a 7 bits value and insert it into the array
-			$bitArray[] = $this->asc2Bin( substr( $message ,$i-1,1) ,7);
+			$bitArray[] = $this->asc2Bin(substr($message, $i - 1, 1), 7);
 			$i++;
 		}
 
@@ -309,24 +319,25 @@ class Pdu
 	 * @param bool $uppercase
 	 * @return string
 	 */
-	private function bit8ToHex($bin, $padding=false, $uppercase=true) {
+	private function bit8ToHex($bin, $padding = false, $uppercase = true)
+	{
 		$hex = '';
 		// Last item for counter (for-loop)
-		$last = strlen($bin)-1;
+		$last = strlen($bin) - 1;
 		// Loop for every item
-		for($i=0; $i<=$last; $i++) {
-			$hex += $bin[$last-$i] * pow(2,$i);
+		for ($i = 0; $i <= $last; $i++) {
+			$hex += $bin[$last - $i] * pow(2, $i);
 		}
 
 		// Convert from decimal to hexadecimal
 		$hex = dechex($hex);
 		// Add a 0 (zero) if there is only 1 value returned, like 'F'
-		if($padding && strlen($hex) < 2 ) {
-			$hex = '0'.$hex;
+		if ($padding && strlen($hex) < 2) {
+			$hex = '0' . $hex;
 		}
 
 		// If we want the output returned as UPPERCASE do this
-		if($uppercase) {
+		if ($uppercase) {
 			$hex = strtoupper($hex);
 		}
 
@@ -337,22 +348,23 @@ class Pdu
 	 * @param string $bits
 	 * @return string
 	 */
-	private function bit7ToHex($bits) {
+	private function bit7ToHex($bits)
+	{
 
 		$i = 0;
 		$hexOutput = '';
 		$running = true;
 
 		// For every 7 bits character array item
-		while($running) {
+		while ($running) {
 
-			if(count($bits)==$i+1) {
+			if (count($bits) == $i + 1) {
 				$running = false;
 			}
 
 			$value = $bits[$i];
 
-			if($value=='') {
+			if ($value == '') {
 				$i++;
 				continue;
 			}
@@ -363,18 +375,18 @@ class Pdu
 			// Default: new value is current value
 			$new = $value;
 
-			if(isset($bits[$i+1])) {
+			if (isset($bits[$i + 1])) {
 				// There is a next array item so make it 8 bits
 				$neededChar = 8 - strlen($value);
 				// Get the char;s from the next array item
-				$charFromNext = substr($bits[$i+1], -$neededChar);
+				$charFromNext = substr($bits[$i + 1], -$neededChar);
 				// Remove used bit's from next array item
-				$bits[$i+1] = substr($bits[$i+1], 0, strlen($bits[$i+1])-$neededChar );
+				$bits[$i + 1] = substr($bits[$i + 1], 0, strlen($bits[$i + 1]) - $neededChar);
 				// New value is characters from next value and current value
-				$new = $charFromNext.$value;
+				$new = $charFromNext . $value;
 			}
 
-			if($new!='') {
+			if ($new != '') {
 				// Always make 8 bits
 				$new = str_pad($new, 8, '0', STR_PAD_LEFT);
 				// The 8 bits to hex conversion
